fcc29ebc959e85f5f2b7761099e82ebcc048760b
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-08-07 21:14:56 +0200

    Added hook debounce

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useDebounce } from "../hooks/debounce";
 import { useSearchUserQuery } from "../store/github/github-api";
 
 export const HomePage = () => {
-  const { isLoading, isError, data } = useSearchUserQuery("vladilen");
+  const [search, setSearch] = useState("");
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUserQuery(debounced, {
+    skip: debounced.length < 3,
+  });
+
+  const handleSearch = () => {};
+
+  useEffect(() => {
+    console.log(debounced);
+  }, [debounced]);
+
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
-      {isError && (
+      {isError ? (
         <p className="text-center text-red-500">Something went wrong...</p>
+      ) : (
+        <div className="relative w-[560px]">
+          <div className="flex">
+            <input
+              type="text"
+              className="border py-2 px-2 w-full h-[42px] mr-5 rounded-md"
+              placeholder="Search for GitHub username..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              onClick={handleSearch}
+              className="border p-2 bg-slate-700 text-violet-50 rounded-md hover:bg-slate-400"
+            >
+              Search
+            </button>
+          </div>
+
+          <ul className="list none absolute top-[42px] left-0 right-0 shadow-md bg-white">
+            {data?.map((user) => (
+              <li key={user.id}>{user.login}</li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
